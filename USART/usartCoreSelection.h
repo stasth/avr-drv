b4c23430ac3a568736b4712309ece5ead010edbe
fred.nadeau <fred.nadeau@3db977c0-08d9-11de-a6e9-d320b29efae0>
2009-03-05 16:41:07 +0000

    Removed the L in XCLK_xxx to math Atmel documentation that use XCK.

--- USART/usartCoreSelection.h
+++ USART/usartCoreSelection.h
@@ -40,19 +40,19 @@
 
 #if defined(__DOXYGEN__)
 #	define USART_NUM_PORT 0 /*!< Number of port available per core */
-#	define XCLK0_DDR DDRA /*!< Data Direction Register for clock source pin of USART 0. */
-#	define XCLK0_DDx DDA0 /*!< Pin index for clock source pin of USART 0. */
-#	define XCLK1_DDR DDRA /*!< Data Direction Register for clock source pin of USART 1. */
-#	define XCLK1_DDx DDA0 /*!< Pin index for clock source pin of USART 1. */
-#	define XCLK2_DDR DDRA /*!< Data Direction Register for clock source pin of USART 2. */
-#	define XCLK2_DDx DDA0 /*!< Pin index for clock source pin of USART 2. */
-#	define XCLK3_DDR DDRA /*!< Data Direction Register for clock source pin of USART 3. */
-#	define XCLK3_DDx DDA0 /*!< Pin index for clock source pin of USART 3. */
+#	define XCK0_DDR DDRA /*!< Data Direction Register for clock source pin of USART 0. */
+#	define XCK0_DDx DDA0 /*!< Pin index for clock source pin of USART 0. */
+#	define XCK1_DDR DDRA /*!< Data Direction Register for clock source pin of USART 1. */
+#	define XCK1_DDx DDA0 /*!< Pin index for clock source pin of USART 1. */
+#	define XCK2_DDR DDRA /*!< Data Direction Register for clock source pin of USART 2. */
+#	define XCK2_DDx DDA0 /*!< Pin index for clock source pin of USART 2. */
+#	define XCK3_DDR DDRA /*!< Data Direction Register for clock source pin of USART 3. */
+#	define XCK3_DDx DDA0 /*!< Pin index for clock source pin of USART 3. */
 #else
 #	if defined (__AVR_ATmega8__)
 #		define USART_NUM_PORT 1
-#		define XCLK0_DDR DDRD
-#		define XCLK0_DDx DDD4
+#		define XCK0_DDR DDRD
+#		define XCK0_DDx DDD4
 #	endif
 #endif
 #endif /* USARTCORESELECTION_H_ */
