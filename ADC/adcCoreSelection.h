2e00195bfcbff318114a018022f4712153d58f3f
fred.nadeau <fred.nadeau@3db977c0-08d9-11de-a6e9-d320b29efae0>
2009-03-26 00:33:00 +0000

    Modification to license Modification to coreSelection

--- ADC/adcCoreSelection.h
+++ ADC/adcCoreSelection.h
@@ -1,3 +1,34 @@
+/* Copyright (c) 2008,2009 Frédéric Nadeau
+   All rights reserved.
+
+   Redistribution and use in source and binary forms,
+   with or without modification, are permitted provided
+   that the following conditions are met:
+
+   1.Redistributions of source code must retain the above
+   copyright notice, this list of conditions and the following
+   disclaimer.
+
+   2.Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+
+   3.Neither the name of the AVR-DRV nor the names of its
+   contributors may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
+
 /*! \file adcCoreSelection.h
  \brief This file identify witch option of the ADC are available for a
  specific AVR device.
@@ -13,39 +44,35 @@
  \li 1.1 Volt reference
 
  \author Frédéric Nadeau
-
- \warning Copyright (c) 2008 Frédéric Nadeau
- All rights reserved.
-
-
- Redistribution and use in source and binary forms, with or without
- modification, are permitted provided that the following conditions
- are met:\n
- 1. Redistributions of source code must retain the above copyright
- notice, this list of conditions and the following disclaimer.\n
- 2. Redistributions in binary form must reproduce the above copyright
- notice, this list of conditions and the following disclaimer in the
- documentation and/or other materials provided with the distribution.\n
- 3. The name of the author nor the names of its contributors may not
- be used to endorse or promote products derived from this software
- without specific prior written permission.
-
- THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
- IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
- INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 #ifndef ADCCORESELECTION_H_
 #define ADCCORESELECTION_H_
 
-//TODO Eliminate ADC_16DigitalInput using ADC_DigitalInput and ADC_16Channel
+#include "avr_mod/io.h" //\TODO If avr-libc accept pin description, change me.
+
+
+#if !(defined(ADCSRA)  || defined(ADMUX)  || defined(ADCW) || defined(ADCH) || defined(ADCL))
+#	error "Device not supported"
+#endif
+
+#ifdef MUX4
+#	define ADC_DiffInput
+#endif
+
+#ifdef MUX5
+#	define ADC_16Channel
+#endif
+
+#ifdef ADATE
+#	define ADC_AutoTrigger
+#endif
+
+#ifdef DIDR0
+#	define ADC_DigitalInput
+#	ifdef DIDR1
+#		define ADC_16DigitalInput
+#	endif
+#endif
 
 #if defined(__DOXYGEN__)
 
@@ -57,26 +84,10 @@
 #	define ADC_DigitalInput		/*!< Device has digital input. */
 
 #else//defined(__DOXYGEN__)
-#	if defined (__AVR_ATmega8__)
-//Most basic driver, no special feature to be enabled
-#	elif defined (__AVR_ATmega32__)
-#		define ADC_DiffInput
-#		define ADC_AutoTrigger
-#	elif defined (__AVR_AT90CAN128__)
-#		define ADC_DiffInput
-#		define ADC_AutoTrigger
-#		define ADC_DigitalInput
-#	elif defined (__AVR_ATmega128__)
-#		define ADC_DiffInput
-#	elif defined (__AVR_ATmega1280__)
-#		define ADC_16DigitalInput
-#		define ADC_DiffInput
-#		define ADC_16Channel
+//I don't know how to detect 1.1V reference based on register
+//Device that has 1.1V has to be added here manually
+#	if defined (__AVR_ATmega1280__)
 #		define ADC_1_1Vref
-#		define ADC_AutoTrigger
-#		define ADC_DigitalInput
-#	else
-#    	error "Device not supported"
 #	endif
 #endif//defined(__DOXYGEN__)
 
--- ADC/adcDef.h
+++ ADC/adcDef.h
@@ -1,38 +1,41 @@
+/* Copyright (c) 2008,2009 Frédéric Nadeau
+   All rights reserved.
+
+   Redistribution and use in source and binary forms,
+   with or without modification, are permitted provided
+   that the following conditions are met:
+
+   1.Redistributions of source code must retain the above
+   copyright notice, this list of conditions and the following
+   disclaimer.
+
+   2.Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+
+   3.Neither the name of the AVR-DRV nor the names of its
+   contributors may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
+
 /*! \file adcDef.h
  \brief This file declares \c typedef used by the ADC module.
 
  Not all part of the following \c enum are available to all devices.
 
  \author Frédéric Nadeau
-
- \warning Copyright (c) 2008 Frédéric Nadeau
- All rights reserved.
-
-
- Redistribution and use in source and binary forms, with or without
- modification, are permitted provided that the following conditions
- are met:\n
- 1. Redistributions of source code must retain the above copyright
- notice, this list of conditions and the following disclaimer.\n
- 2. Redistributions in binary form must reproduce the above copyright
- notice, this list of conditions and the following disclaimer in the
- documentation and/or other materials provided with the distribution.\n
- 3. The name of the author nor the names of its contributors may not
- be used to endorse or promote products derived from this software
- without specific prior written permission.
-
- THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
- IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
- INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 #ifndef ADCDEF_H_
 #define ADCDEF_H_
 
@@ -284,7 +287,7 @@ typedef enum ADC_DigitalChannel_e
 	ADC_DigChan5 = 0x0020,
 	ADC_DigChan6 = 0x0040,
 	ADC_DigChan7 = 0x0080,
-#	if defined(ADC_16Channel)
+#	if defined(ADC_16DigitalInput)
 	ADC_DigChan8 = 0x0100,
 	ADC_DigChan9 = 0x0200,
 	ADC_DigChan10 = 0x0400,
@@ -295,8 +298,8 @@ typedef enum ADC_DigitalChannel_e
 	ADC_DigChan15 = 0x8000
 #	endif
 }ADC_DigitalChannel_t;
-#endif
-#endif
+#endif//defined(ADC_DigitalInput)
+#endif//defined(__DOXYGEN__)
 
 
 #endif /*ADCDEF_H_*/
