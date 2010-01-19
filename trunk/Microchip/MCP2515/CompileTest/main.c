39791cd9df6ba7c36347147f7a1fe030ce604591
fred.nadeau <fred.nadeau@3db977c0-08d9-11de-a6e9-d320b29efae0>
2010-01-19 02:03:38 +0000

    Initial commit

---
+++ trunk/Microchip/MCP2515/CompileTest/main.c
@@ -0,0 +1,20 @@
+#include "MCP2515.h"
+
+void mcp2515_spi_port_init(void)
+{}
+
+void mcp2515_spi_select(void)
+{}
+
+void mcp2515_spi_unselect(void)
+{}
+
+void mcp2515_spi_transfer(uint8_t dataOut, uint8_t *dataIn)
+{}
+
+int  main (void)
+{
+
+    return 0;
+}
+
