a7c652f37de246f862503526fe238fd169dd51af
fred.nadeau <fred.nadeau@3db977c0-08d9-11de-a6e9-d320b29efae0>
2010-07-30 00:26:57 +0000

    The examples now link againt the lib.

--- example/ADC/ATmega128/main.c
+++ example/ADC/ATmega128/main.c
@@ -1,50 +1,48 @@
-/*
- //TODO Enter description
+/* Copyright (c) 2008,2010 Frédéric Nadeau
+   All rights reserved.
 
- \author Frédéric Nadeau
+   Redistribution and use in source and binary forms,
+   with or without modification, are permitted provided
+   that the following conditions are met:
 
- \warning Copyright (c) 2008 Frédéric Nadeau
- All rights reserved.
+   1.Redistributions of source code must retain the above
+   copyright notice, this list of conditions and the following
+   disclaimer.
 
+   2.Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
 
- Redistribution and use in source and binary forms, with or without
- modification, are permitted provided that the following conditions
- are met:\n
- 1. Redistributions of source code must retain the above copyright
- notice, this list of conditions and the following disclaimer.\n
- 2. Redistributions in binary form must reproduce the above copyright
- notice, this list of conditions and the following disclaimer in the
- documentation and/or other materials provided with the distribution.\n
- 3. The name of the author nor the names of its contributors may not
- be used to endorse or promote products derived from this software
- without specific prior written permission.
+   3.Neither the name of the AVR-DRV nor the names of its
+   contributors may be used to endorse or promote products derived
+   from this software without specific prior written permission.
 
- THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
- IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
- INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
 
-#include "avr-drv.h"
+#include "adc.h"
 
 int main(void)
 {
-	adcLeftAdjust(true);
-	adcSelectVref(ADC_VrefAtAREF);
-	adcPrescalerSelection(ADC_DivFactor64);
-	adcSelectInput(ADC_Chan_1);
-	adcTriggerSource(ADC_FreeRunning);
-	adcTriggerEnable(true);
-	adcInterruptEnable(false);
-	adcEnable(true);
-	adcStartConversion();
+    adc_left_adjust(true);
+    adc_select_vref(adc_voltage_ref_aref);
+    adc_prescaler_selection(adc_prescaler_64);
+    adc_select_input(adc_input_channel_0);
+    adc_set_trigger_source(adc_trigger_source_free_running);
+    adc_trigger_enable(true);
+    adc_interrupt_enable(false);
+    adc_enable(true);
+    adc_start_conversion();
 
-	for(;;);
-	return 0;
+    for(;;);
+    return 0;
 }
