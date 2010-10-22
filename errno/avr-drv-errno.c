10ca50bac77e882313c93478f0b94d5a242499fb
fred.nadeau <fred.nadeau@3db977c0-08d9-11de-a6e9-d320b29efae0>
2010-10-22 23:56:34 +0000

    Fix typo in global variable.

--- errno/avr-drv-errno.c
+++ errno/avr-drv-errno.c
@@ -30,4 +30,4 @@
    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
 
-volatile unsigned char errno_avrdrv = 0;
+volatile unsigned char avr_drv_errno = 0;
