42548d5d2337ae0cdfc03e64335b0381aac4ef6f
fred.nadeau <fred.nadeau@gmail.com>
2011-07-05 21:16:53 +0000

    Added Doxygen tags Added compile switch for sense control since not all device have the same features

--- src/extint.h
+++ src/extint.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2010 Frédéric Nadeau
+/* Copyright (c) 2010-2011 Frédéric Nadeau
    All rights reserved.
 
    Redistribution and use in source and binary forms,
@@ -28,17 +28,47 @@
    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
+/*! \file extint.h
+ \brief Function definition for External Interrupt module.
 
+ \todo Add support for device with fewer than 8 external interrupt
+ \todo Validate any edge capability for other devices
+
+ \author Frédéric Nadeau
+ */
 #ifndef __EXTINT_H_
 #define __EXTINT_H_
 
+#if defined(__DOXYGEN__)
+/*! List of external interrupt sense control.
+ *
+ * Some device do not implement all possibility.
+ * */
+enum extint_sense_ctrl_e
+{
+    extint_sense_ctrl_lowLevel, /*!< Low level on INT0 pin generates an interrupt request. */
+    extint_sense_ctrl_anyEdge, /*!< Any level change on INT0 pin generates an interrupt request. */
+    extint_sense_ctrl_fallingEdge, /*!< Change from 1 to 0 on INT0 pin generates an interrupt request. */
+    extint_sense_ctrl_risingEdge /*!< Change from 0 to 1 on INT0 pin generates an interrupt request. */
+};
+
+/*! List of external interrupt sense control typedef. */
+typedef extint_sense_ctrl_e extint_sense_ctrl_t;
+#else
 typedef enum extint_sense_ctrl_e
 {
-    extint_sense_ctrl_lowLevel,
+    extint_sense_ctrl_lowLevel = 0,
+#if defined(__AVR_ATmega640__) \
+|| defined(__AVR_ATmega1280__) \
+|| defined(__AVR_ATmega1281__) \
+|| defined(__AVR_ATmega2560__) \
+|| defined(__AVR_ATmega2561__)
     extint_sense_ctrl_anyEdge,
-    extint_sense_ctrl_fallingEdge,
+#endif
+    extint_sense_ctrl_fallingEdge = 2,
     extint_sense_ctrl_risingEdge
-} extint_sense_ctrl_t;
+}extint_sense_ctrl_t;
+#endif
 
 void extint_set_sense_control(extint_sense_ctrl_t senseCtrl, uint8_t pin);
 
